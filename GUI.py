4e70eb50484e5023d614d7ab8d0ddc223250bab9
OllieBL <oliver.black4@education.nsw.gov.au>
2025-03-19 10:09:55 +1100

    Continued GUI file and continued functions file
    added all kinds of api search to function file, and I've added more to the GUI file, most notable I have started to add other variables to save the location data, as well as adding the other button to access forecast

--- GUI.py
+++ GUI.py
@@ -1,40 +1,59 @@
 from tkinter import *
 from functions import *
 
+
 class Weather_App:
     def __init__(self, master):
         self.master = master
-        self.master.geometry("200x200")
-        self.firstPage()
+        self.master.geometry("1000x500")
+        self.APISelection()
+
+        # These variables are string variables that allow the location and api key to be used in other parts of the code
         self.location_var = StringVar()
         self.apiKey_var = StringVar()
+        self.locationData = ''
 
-    def firstPage(self):
+
+    # This function is the first selection page for the user, it allows the user to pick what API they want to search; current, history, or forecast
+    def APISelection(self):
+
+        # This for loop clears the window for the new page
         for i in self.master.winfo_children():
             i.destroy()
-        self.frame1 = Frame(self.master, width=200, height=200)
+
+        
+        self.frame1 = Frame(self.master, width=1000, height=500)
         self.frame1.pack()
-        self.button1 = Button(self.frame1, text='Go to page 2', command=self.present)
-        self.button1.pack()
+        self.currentButton = Button(self.frame1, text='Current API', command=self.current)
+        self.currentButton.pack()
+        self.forecastButton = Button(self.frame1, text='Forecast API', command=self.current)
+        self.forecastButton.pack()
 
-    def present(self):
+
+    # This function is the page that allows the user to select the location that they wnat to search for when they choose the 'current' API
+    def current(self):
         for i in self.master.winfo_children():
             i.destroy()
-        self.frame2 = Frame(self.master, width=200, height=200)
+        self.frame2 = Frame(self.master, width=1000, height=500)
         self.frame2.pack()
         self.locationEntry = Entry(self.frame2, textvariable=self.location_var)
         self.locationEntry.pack()
         self.APIKeyEntry = Entry(self.frame2, textvariable=self.apiKey_var)
         self.APIKeyEntry.pack()
-        self.submitButton = Button(self.frame2, text='Submit', command=self.end)
+        self.submitButton = Button(self.frame2, text='Submit', command=self.functionsRunner)
         self.submitButton.pack()
     
-    def end(self):
+
+    # This function is a collection of the various functions that the code will run from the functions.py file
+    def functionsRunner(self):
         apiKey = self.apiKey_var.get()
         location = self.location_var.get()
-        print(Search_API(apiKey, location, 'current'))
+        self.locationData = CurrentAPISearch(apiKey, location)
+        print(self.locationData)
 
+def runGUI():
+    root = Tk()
+    Weather_App(root)
+    root.mainloop()
 
-root = Tk()
-Weather_App(root)
-root.mainloop()
+runGUI()
--- functions.py
+++ functions.py
@@ -1,7 +1,16 @@
 import requests
 
-def Search_API(APIKey, location, referenceType):
-    response = requests.get(f'http://api.weatherapi.com/v1/{referenceType}.json?key={APIKey}&q={location}')
+def CurrentAPISearch(APIKey, location):
+    response = requests.get(f'http://api.weatherapi.com/v1/current.json?key={APIKey}&q={location}')
     locationData = response.json()
     return locationData
 
+def ForecastAPISearch(APIKey, location):
+    response = requests.get(f'http://api.weatherapi.com/v1/forecast.json?key={APIKey}&q={location}&days=7')
+    locationData = response.json()
+    return locationData
+
+def HistoryAPISearch(APIKey, location, date):
+    response = requests.get(f'http://api.weatherapi.com/v1/history.json?key={APIKey}&q={location}&date=2025-{date[0]}-{date[1]}')
+    locationData = response.json()
+    return locationData
