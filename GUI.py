753c2e0bb5439dbc79c5d7c796550c9517938fb0
OllieBL <oliver.black4@education.nsw.gov.au>
2025-03-24 06:30:17 +1100

    Made GUI display returned data
    The GUI now returns the data retrieved from the api, and function now has a new function to convert it from json into a list to display easier

--- GUI.py
+++ GUI.py
@@ -21,28 +21,62 @@ class Weather_App:
         for i in self.master.winfo_children():
             i.destroy()
 
-        
+        # This section is all the components
+        # This line creates and packs the frame
         self.frame1 = Frame(self.master, width=1000, height=500)
         self.frame1.pack()
+        # This line creates and packs the button to select the current API 
         self.currentButton = Button(self.frame1, text='Current API', command=self.current)
         self.currentButton.pack()
+        # This line creates and packs the button to select the forecast API
         self.forecastButton = Button(self.frame1, text='Forecast API', command=self.current)
         self.forecastButton.pack()
 
 
     # This function is the page that allows the user to select the location that they wnat to search for when they choose the 'current' API
     def current(self):
+
+        # This for loop deletes the previous page for the new page to go on top of
         for i in self.master.winfo_children():
             i.destroy()
+
+
+        # This section creates page and all the components on it
+        # This line creates and packs the frame
         self.frame2 = Frame(self.master, width=1000, height=500)
         self.frame2.pack()
+        # This line creates and packs the text box for the user to enter the location
         self.locationEntry = Entry(self.frame2, textvariable=self.location_var)
         self.locationEntry.pack()
+        # This line creates and packs the text box for the user to enter their API key
         self.APIKeyEntry = Entry(self.frame2, textvariable=self.apiKey_var)
         self.APIKeyEntry.pack()
-        self.submitButton = Button(self.frame2, text='Submit', command=self.functionsRunner)
+        # This line is the button that the user clicks on to confirm their location choice and API key
+        self.submitButton = Button(self.frame2, text='Submit', command=lambda: [self.functionsRunner(), self.displayCurrent()])
         self.submitButton.pack()
-    
+
+    def displayCurrent(self):
+        
+        locationData = makeDataReadable(self.locationData)
+
+        # This for loop deletes the previous page for the new page to go on top of
+        for i in self.master.winfo_children():
+            i.destroy()
+
+        # This section creates page and all the components on it
+        # This line creates and packs the frame
+        self.frame4 = Frame(self.master, width=1000, height=500)
+        self.frame4.pack()
+
+        #
+        for i in range(len(locationData)):
+            for j in range(len(locationData[i])):
+                self.currentTable = Entry(self.frame4, width=50)
+                self.currentTable.grid(row=j, column=i)
+                self.currentTable.insert(END, locationData[i][j])
+
+
+        
 
     # This function is a collection of the various functions that the code will run from the functions.py file
     def functionsRunner(self):
@@ -55,5 +89,6 @@ def runGUI():
     root = Tk()
     Weather_App(root)
     root.mainloop()
+
 
 runGUI()
--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import requests
 
 def CurrentAPISearch(APIKey, location):
-    response = requests.get(f'http://api.weatherapi.com/v1/current.json?key={APIKey}&q={location}')
+    response = requests.get(f'http://api.weatherapi.com/v1/current.json?key=b2b6f09cd9d943fdab584210251803&q={location}')
     locationData = response.json()
     return locationData
 
@@ -14,3 +14,16 @@ def HistoryAPISearch(APIKey, location, date):
     response = requests.get(f'http://api.weatherapi.com/v1/history.json?key={APIKey}&q={location}&date=2025-{date[0]}-{date[1]}')
     locationData = response.json()
     return locationData
+
+
+def makeDataReadable(locationData):
+    out = [[],[]]
+    locationData['current'].pop('condition')
+    for i in locationData.values():
+        for j in i:
+            out[0].append(j)
+            out[1].append(i[j])
+
+    return out
+
+print(HistoryAPISearch('b2b6f09cd9d943fdab584210251803', 'Terrigal', [3, 20]))
