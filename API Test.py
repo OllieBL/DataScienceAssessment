69896f9baf80fbb5152709ce2d20dc3e23e85f88
OllieBL <oliver.black4@education.nsw.gov.au>
2025-04-10 06:59:22 +1000

    Removed API test file, added the rest

--- API Test.py
+++
@@ -1,7 +0,0 @@
-import requests 
-
-location = input('location')
-
-response = requests.get(f'http://api.weatherapi.com/v1/forecast.json?key=b2b6f09cd9d943fdab584210251803&q={location}&days=2')
-data = response.json()
-print(data)
